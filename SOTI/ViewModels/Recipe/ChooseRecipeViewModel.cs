c53c99af4596e5696d202c58e2c1048322316e58
Mattia Brusamento <bmattia92@gmail.com>
2015-12-13 15:18:05 +0100

    ChooseRecipe

--- SOTI/ViewModels/Recipe/ChooseRecipeViewModel.cs
+++ SOTI/ViewModels/Recipe/ChooseRecipeViewModel.cs
@@ -1,27 +1,57 @@
 ﻿using System.Windows;
 using Caliburn.Micro;
 using SOTI.Message;
+using SOTI.Model;
+using System;
 
 namespace SOTI.ViewModels.Recipe
 {
     public class ChooseRecipeViewModel : BaseGameScreenViewModel
     {
+        private DataLayer data;
+        private int currentRecipe;
         /// <summary>
         /// Costrutto della classe, per far funzionare il tutto è necessario chiamare anche il costruttore della classe base
         /// </summary>
         /// <param name="eventAggregator"></param>
-        public ChooseRecipeViewModel(IEventAggregator eventAggregator) : base(eventAggregator)
+        public ChooseRecipeViewModel(IEventAggregator eventAggregator, DataLayer data) : base(eventAggregator)
         {
-
+            this.data = data;
+            Random rnd = new Random();
+            int firstRecipe = rnd.Next(data.ricette.Count - 1);
+            currentRecipe = firstRecipe;
+            showRecipe(data.ricette[currentRecipe]);
+            
         }
 
-        public override Visibility GreenButtonVisibility { get { return Visibility.Hidden; } }
+        /// <summary>
+        /// Method that should modify the interface in order to change the shown Recipe.
+        /// </summary>
+        /// <param name="ricetta"></param>
+        private void showRecipe(Ricetta ricetta)
+        {
+            ;
+        }
 
-        public override void Handle(RedButtonMessage message)
+        public override Visibility BlueButtonVisibility { get { return Visibility.Hidden; } }
+
+        public override void Handle(GreenButtonMessage message)
         {
             //Navigate to the RecipeStep Screen
             this.NavigateToScreen<RecipeStepViewModel>();
             base.Handle(message);
         }
+
+        public override void Handle(RedButtonMessage message)
+        {
+            currentRecipe++;
+            if (currentRecipe >= data.ricette.Count)
+            {
+                currentRecipe = 0;
+            }
+            showRecipe(data.ricette[currentRecipe]);
+            base.Handle(message);
+        }
+
     }
 }
