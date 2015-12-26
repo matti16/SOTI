9aa8d5cbffb46562475e2eba2702c35d7791c499
Mattia Brusamento <bmattia92@gmail.com>
2015-12-26 19:31:27 +0100

    Grafica Recipe Step

--- SOTI/ViewModels/Recipe/RecipeStepViewModel.cs
+++ SOTI/ViewModels/Recipe/RecipeStepViewModel.cs
@@ -6,9 +6,9 @@ using SOTI.Message;
 
 namespace SOTI.ViewModels.Recipe
 {
-    public class RecipeStepViewModel : BaseGameScreenViewModel
+    public class RecipeStepViewModel : BaseGameScreenViewModel, IHandle<GUIReadyMessage>
     {
-        private readonly Passo passo;
+        private Passo passo;
         private readonly StateRicetta state;
 
         public RecipeStepViewModel(IEventAggregator eventAggregator, StateRicetta state) : base(eventAggregator)
@@ -17,30 +17,26 @@ namespace SOTI.ViewModels.Recipe
             this.passo = state.PassoCorrente;
             this.Ingredienti = new ObservableCollection<Cibo>();
 
+            showPasso();
+        }
+
+        private void showPasso()
+        { 
             if (passo.passoDoppio)
             {
                 Random rnd = new Random();
                 if (rnd.Next(2) == 1)
                 {
-                    //Metto prima quello sbagliato
-                    this.Ingredienti.Add(passo.ciboSbagliato);
-                    //Poi quello giusto
-                    this.Ingredienti.Add(passo.ciboGiusto);
+                    this.eventAggregator.PublishOnUIThread(new PassoMessage(true, passo.ciboGiusto, passo.ciboSbagliato));
                 }
                 else
                 {
-                    //Metto prima quello giusto
-                    this.Ingredienti.Add(passo.ciboGiusto);
-                    //Poi quello sbagliato
-                    this.Ingredienti.Add(passo.ciboSbagliato);
+                    this.eventAggregator.PublishOnUIThread(new PassoMessage(true, passo.ciboSbagliato, passo.ciboGiusto));
                 }
-                this.Giusto = passo.ciboGiusto.nome;
-                this.Sbagliato = passo.ciboSbagliato.nome;
             }
             else
             {
-                this.Ingredienti.Add(passo.ciboGiusto);
-                this.Giusto = passo.ciboGiusto.nome;
+                this.eventAggregator.PublishOnUIThread(new PassoMessage(false, passo.ciboGiusto, null));
             }
         }
 
@@ -50,12 +46,13 @@ namespace SOTI.ViewModels.Recipe
         {
             if (message.Food == this.passo.ciboGiusto.id.ToString())
             {
-                this.Risultato = "Bravo";
+                
                 System.Threading.Thread.Sleep(1000);
                 if (this.state.HasNext)
                 {
                     this.state.MoveNext();
-                    this.NavigateToScreen<RecipeStepViewModel>();
+                    this.passo = state.PassoCorrente;
+                    showPasso();
                 }
                 else
                 {
@@ -64,53 +61,14 @@ namespace SOTI.ViewModels.Recipe
             }
             else
             {
-                this.Risultato = "NOOOO";
+                
             }
             base.Handle(message);
         }
 
-        private string giusto = "";
-        public virtual string Giusto
+        public void Handle(GUIReadyMessage message)
         {
-            get { return giusto; }
-            set
-            {
-                if (giusto != value)
-                {
-                    giusto = value;
-                    NotifyOfPropertyChange<string>(() => Giusto);
-                }
-            }
+            showPasso();
         }
-
-        private string sbagliato = "";
-        public virtual string Sbagliato
-        {
-            get { return sbagliato; }
-            set
-            {
-                if (sbagliato != value)
-                {
-                    sbagliato = value;
-                    NotifyOfPropertyChange<string>(() => Sbagliato);
-                }
-            }
-        }
-
-        private string risultato = "";
-        public virtual string Risultato
-        {
-            get { return risultato; }
-            set
-            {
-                if (risultato != value)
-                {
-                    risultato = value;
-                    NotifyOfPropertyChange<string>(() => Risultato);
-                }
-            }
-        }
-
-        
     }
 }
--- SOTI/Views/Recipe/RecipeStepView.xaml.cs
+++ SOTI/Views/Recipe/RecipeStepView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using SOTI.Message;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,11 +20,70 @@ namespace SOTI.Views.Recipe
     /// <summary>
     /// Interaction logic for RecipeStepView.xaml
     /// </summary>
-    public partial class RecipeStepView : UserControl
+    public partial class RecipeStepView : UserControl, IHandle<PassoMessage>
     {
+        private readonly IEventAggregator eventAggregator;
+        private string baseUri = @"pack://application:,,,/SOTI;component/Media/Images/Cibi/";
+
         public RecipeStepView()
         {
             InitializeComponent();
+            this.eventAggregator = AppBootstrapper.container.GetInstance<IEventAggregator>();
+            eventAggregator.Subscribe(this);
+
+            //Load Video
+            this.CenterMedia.Source = new Uri(VideoUri.Video + VideoUri.Cuocolo + VideoUri.Think, UriKind.Relative);
+            this.CenterBackMedia.Source = new Uri(VideoUri.Video + VideoUri.Cuocolo + VideoUri.Blink, UriKind.Relative);
+
+            //Handlers
+            CenterMedia.MediaEnded += CenterMedia_MediaEnded;
+            CenterBackMedia.MediaEnded += CenterBackMedia_MediaEnded;
+
+            //Play
+            CenterMedia.Play();
+            CenterBackMedia.Play();
+
+            this.eventAggregator.PublishOnUIThread(new GUIReadyMessage());
+        }
+
+        private void CenterBackMedia_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            CenterBackMedia.Position = TimeSpan.Zero;
+            CenterBackMedia.Play();
+        }
+
+        private void CenterMedia_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            CenterMedia.Position = TimeSpan.Zero;
+            CenterMedia.Play();
+        }
+
+        /// <summary>
+        /// Method to update the view with the requested ingredients for the recipe.
+        /// </summary>
+        /// <param name="message"></param>
+        public void Handle(PassoMessage message)
+        {
+            if (message.doble)
+            {
+                PassoSingolo_Grid.Visibility = Visibility.Hidden;
+
+                UpIngredient_Txt.Text = message.first.nome;
+                //UpIngredient_Img.Source = new BitmapImage(new Uri(baseUri + message.first.immagine));
+
+                BotIngredient_Txt.Text = message.second.nome;
+                //BotIngredient_Img.Source = new BitmapImage(new Uri(baseUri + message.second.immagine));
+
+                PassoDoppio_Grid.Visibility = Visibility.Visible;
+
+            }
+            else
+            {
+                PassoDoppio_Grid.Visibility = Visibility.Hidden;
+                Ingredient_Txt.Text = message.first.nome;
+                //Ingredient_Img.Source = new BitmapImage(new Uri(baseUri + message.first.immagine));
+                PassoSingolo_Grid.Visibility = Visibility.Visible;               
+            }
         }
     }
 }
